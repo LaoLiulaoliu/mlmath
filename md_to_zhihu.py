44a55b07f7c32d3f6b852f428ea740ffa34d3ba3
laoliu <q_0_0_p@163.com>
2020-09-28 11:42:19 +0800

    a

--- md_to_zhihu.py
+++ md_to_zhihu.py
@@ -5,14 +5,13 @@ import re
 import contextlib
 
 def replace(content):
-    dollar_double = '\n<img src="https://www.zhihu.com/equation?tex=\1\\\\" alt="\1\\\\" class="ee_img tr_noresize" eeimg="1">\n'
-    r_double = re.compile('\$\$\n*(.*?)\n*\$\$', re.DOTALL)
+    dollar_double = r'\n<img src="https://www.zhihu.com/equation?tex=\1\\\\" alt="\1\\\\" class="ee_img tr_noresize" eeimg="1">\n'
+    r_double = re.compile(r'\$\$\n*(.*?)\n*\$\$', re.DOTALL)
 
-    dollar = '\n<img src="https://www.zhihu.com/equation?tex=\1" alt="\1" class="ee_img tr_noresize" eeimg="1">\n'
-    r = re.compile('\$\n*(.*?)\n*\$', re.DOTALL)
+    dollar = r'\n<img src="https://www.zhihu.com/equation?tex=\1" alt="\1" class="ee_img tr_noresize" eeimg="1">\n'
+    r = re.compile(r'\$\n*(.*?)\n*\$', re.DOTALL)
 
     content_new = r_double.sub(dollar_double, content)
-    return content_new
     return r.sub(dollar, content_new)
 
 
