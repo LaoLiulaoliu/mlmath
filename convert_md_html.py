d1c227842e541eedb526329750befadf751ac84a
LaoLiuLaoliu <miraclecome@gmail.com>
2021-01-16 18:05:54 +0800

    add

---
+++ convert_md_html.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+import sys
+import markdown
+
+if __name__ == '__main__':
+    name, postfix = sys.argv[1].split('.')
+    with open(name + '.' + postfix) as fd_r, open(name + '.html', 'w') as fd_w:
+        content = fd_r.read()
+        html = markdown.markdown(content)
+        fd_w.write(html)
